9c1a1fb37ae2c1f084d7453356cfcafe368f778d
llmh3624 <llmh3624@gmail.com>
2023-07-08 01:57:09 +0200

    Added meaning pt1

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState({});
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0]);
+    setResults(response.data.meanings[0].definition);
+
   }
   function search(event) {
     event.preventDefault();
@@ -24,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Results (props){
+    return(
+        <div className="Results">
+            Hello from Results
+        </div>
+    )
+}
