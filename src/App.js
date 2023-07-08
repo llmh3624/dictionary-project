f7df515fdaf575cc2217f7049e8d75abcd38993c
llmh3624 <llmh3624@gmail.com>
2023-07-08 14:37:02 +0200

    Added meanings, synonyms, photos and phonetics

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1>Hello World</h1>
+          <h1>DICTIONARY</h1>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunrise" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Laura Moragues</small>
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://heroic-palmier-0df45c.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Laura Moragues
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://github.com/llmh3624/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and hosted on Netlify
+          </small>
         </footer>
       </div>
     </div>
